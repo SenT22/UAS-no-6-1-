420a5284a8a3278bac1661de3523919eadb76e3a
SenT22 <115127692+SenT22@users.noreply.github.com>
2023-01-19 11:54:56 +0700

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,11 @@
+public class pattern1 {
+    public static void main(String[]agrs){
+        for (int a = 1; a <= 5; a++ ){
+            for(int b = 1; b <= a ; b++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
